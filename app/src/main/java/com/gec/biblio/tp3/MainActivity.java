3b70196042ad85fd185bf2b216d007223e458b16
weyller <weyller@gmail.com>
2017-08-29 22:01:53 -0400

    1st commit

---
+++ app/src/main/java/com/gec/biblio/tp3/MainActivity.java
@@ -0,0 +1,24 @@
+package com.gec.biblio.tp3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        setContentView(R.layout.activity_main);
+
+
+        setTitle("Biblio GEC");
+
+
+
+
+    }
+}
